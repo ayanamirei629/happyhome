f8dcf70497a9c35c2a66364aa6fba3fa61e4070a
Craig <ayanamirei629@gmail.com>
2022-11-07 13:00:33 +0800

    test4

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -9,11 +9,13 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+echo 'www.ffhome.vip' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git push -f https://github.com/ayanamirei629/ayanamirei629.github.io.git master/docs
+git push -f https://github.com/ayanamirei629/ayanamirei629.github.io.git master
 
 cd -
